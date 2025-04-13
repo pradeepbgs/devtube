27960a02bf13c6c0247a7fd6933df46e97ef1a55
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-04-13 15:12:55 +0530

    will impleemnt rate limiting

--- authservice/src/routes/user.route.ts
+++ authservice/src/routes/user.route.ts
@@ -2,6 +2,7 @@ import {Diesel} from 'diesel-core'
 import { UserRepository } from '../repository/user.repository'
 import { UserService } from '../service/user.service'
 import { UserController } from '../controller/user.controller'
+import { fileSaveMiddleware } from '../middleware/saveFile'
 
 const userRouter = new Diesel()
 
@@ -10,7 +11,7 @@ const userService = UserService.getInstance(userRepository)
 const userController = UserController.getInstance(userService)
 
 
-userRouter.put("/update", userController.UpdateUser)
+userRouter.put("/update",fileSaveMiddleware , userController.UpdateUser)
 userRouter.get("/user", userController.GetUser)
 
 
